5aab96b113b00d2f06105ab9837db91ea2e8c2e3
Milana <milaana1988@gmail.com>
2025-02-05 22:26:04 +0200

    package.json

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { BandFormValues } from "../hooks/useBandForm";
 
-const baseURL = "http://127.0.0.1:8000";
+const baseURL = "https://my-new-fav-band-5d12579f2ef9.herokuapp.com";
 
 const api = axios.create({
   baseURL: baseURL,
